f8c94e9f9a2e6eeca4250d7caa53a4153749fe71
HP <HP@DESKTOP-7MPP6VI>
2024-10-22 17:35:23 +0530

    initial commit

---
+++ app62/src/main/java/com/nit/App62Application.java
@@ -0,0 +1,14 @@
+package com.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App62Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App62Application.class, args);
+		System.out.println("durga prasad is a good boy");
+	}
+
+}
